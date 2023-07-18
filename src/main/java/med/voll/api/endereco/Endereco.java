1524c5ab0e343bb7b16a9252d8ea47c4fb06f4b8
rafaelmendesJW <rafaelmendes@acad.ifma.edu.br>
2023-07-18 00:38:20 -0300

    Inserindo delete e update.

--- src/main/java/med/voll/api/endereco/Endereco.java
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -20,14 +20,39 @@ public class Endereco {
     private String uf;
 
     public Endereco(DadosEndereco dados) {
-    this.logradouro = dados.logradouro();
-    this.bairro = dados.bairro();
-    this.cep = dados.cep();
-    this.complemento = dados.complemento();
-    this.numero = dados.numero();
-    this.cidade=dados.cidade();
-    this.uf= dados.uf();
+        this.logradouro = dados.logradouro();
+        this.bairro = dados.bairro();
+        this.cep = dados.cep();
+        this.complemento = dados.complemento();
+        this.numero = dados.numero();
+        this.cidade=dados.cidade();
+        this.uf= dados.uf();
 
 
     }
+
+    public void atualizarInformacoes(DadosEndereco dados) {
+        if(dados.logradouro()!= null){
+            this.logradouro = dados.logradouro();
+        } 
+        if(dados.bairro()!= null){
+            this.bairro = dados.bairro();
+        }
+        if(dados.cep()!= null){
+            this.cep = dados.cep();
+        } 
+        if(dados.uf()!= null){
+            this.uf = dados.uf();
+        } 
+        if(dados.cidade()!= null){
+            this.cidade = dados.cidade();
+        }
+        if(dados.numero()!= null){
+            this.numero = dados.numero();
+        } 
+        if(dados.complemento()!= null){
+            this.complemento = dados.complemento();
+        }  
+    }
+
 }
