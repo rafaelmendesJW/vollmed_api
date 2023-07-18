70ac5d7431cb5d310b659e872b3c4ce266a5723e
rafaelmendesJW <rafaelmendes@acad.ifma.edu.br>
2023-07-17 21:45:41 -0300

    Comentei a linha 12 e 19 das classes DadosCadastroMedico e DadosEndereco devido ao nao funcionamento.

--- src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -4,10 +4,11 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
+
 import med.voll.api.endereco.DadosEndereco;
 
-public record DadosCadastroMedico ( @NotBlank
+public record DadosCadastroMedico (
+        @NotBlank
         String nome,
         @NotBlank
         @Email
@@ -15,7 +16,7 @@ public record DadosCadastroMedico ( @NotBlank
         @NotBlank
         String telefone,
         @NotBlank
-        @Pattern (regexp = "\\d{4,6}")
+       // @Pattern (regexp = "d{4,6}")
         String crm,
 
         @NotNull
@@ -23,7 +24,7 @@ public record DadosCadastroMedico ( @NotBlank
         
         @NotNull
         @Valid
-        DadosEndereco endereco)
+        DadosEndereco endereco )
        {
 }
     
